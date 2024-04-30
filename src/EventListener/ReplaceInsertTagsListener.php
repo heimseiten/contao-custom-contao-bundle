d1505256ac8fcdb7533247601f28eefdfa7ffb5f
Niels <info@heimseiten.de>
2024-04-30 13:00:01 +0200

    Update ReplaceInsertTagsListener.php

--- src/EventListener/ReplaceInsertTagsListener.php
+++ src/EventListener/ReplaceInsertTagsListener.php
@@ -12,36 +12,35 @@ class ReplaceInsertTagsListener
     {
         [$name, $param] = explode('::', $tag) + [null, null];
 
-
         switch ($name) {
             
             case 'fab':
-                return file_get_contents('assets/fontawesome/svgs/brands/'.$param.'.svg');
+                return '<i class="fa">'.file_get_contents('assets/fontawesome/svgs/brands/'.$param.'.svg').'</i>';
                 break;
             case 'fa':
-                return file_get_contents('assets/fontawesome/svgs/regular/'.$param.'.svg');
+                return '<i class="fa">'.file_get_contents('assets/fontawesome/svgs/regular/'.$param.'.svg').'</i>';
                 break;
             case 'fas':
-                return file_get_contents('assets/fontawesome/svgs/solid/'.$param.'.svg');
+                return '<i class="fa">'.file_get_contents('assets/fontawesome/svgs/solid/'.$param.'.svg').'</i>';
                 break;
             
             case 'fad':
-                return file_get_contents('files/layout/icons/fontawesome/svgs/duotone/'.$param.'.svg');
+                return '<i class="fa">'.file_get_contents('files/layout/icons/fontawesome/svgs/duotone/'.$param.'.svg').'</i>';
                 break;
             case 'fal':
-                return file_get_contents('files/layout/icons/fontawesome/svgs/light/'.$param.'.svg');
+                return '<i class="fa">'.file_get_contents('files/layout/icons/fontawesome/svgs/light/'.$param.'.svg').'</i>';
                 break;
             case 'fat':
-                return file_get_contents('files/layout/icons/fontawesome/svgs/thin/'.$param.'.svg');
+                return '<i class="fa">'.file_get_contents('files/layout/icons/fontawesome/svgs/thin/'.$param.'.svg').'</i>';
                 break;
             case 'fasl':
-                return file_get_contents('files/layout/icons/fontawesome/svgs/sharp-light/'.$param.'.svg');
+                return '<i class="fa">'.file_get_contents('files/layout/icons/fontawesome/svgs/sharp-light/'.$param.'.svg').'</i>';
                 break;
             case 'fasr':
-                return file_get_contents('files/layout/icons/fontawesome/svgs/sharp-regular/'.$param.'.svg');
+                return '<i class="fa">'.file_get_contents('files/layout/icons/fontawesome/svgs/sharp-regular/'.$param.'.svg').'</i>';
                 break;
             case 'fass':
-                return file_get_contents('files/layout/icons/fontawesome/svgs/sharp-solid/'.$param.'.svg');
+                return '<i class="fa">'.file_get_contents('files/layout/icons/fontawesome/svgs/sharp-solid/'.$param.'.svg').'</i>';
                 break;
 
             default:
