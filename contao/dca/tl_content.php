4c8770b64dbaceb81525fc67885a113d8ecd571c
Niels <info@heimseiten.de>
2024-06-02 12:05:51 +0200

    Update tl_content.php

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -15,21 +15,25 @@ PaletteManipulator::create()
     
     ->addField('sliderBreakpoint1', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('sliderSlidesPerViewBreakpoint1', 'slider_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('sliderSlidesPerGroupBreakpoint1', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('sliderSpaceBetweenBreakpoint1', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('sliderCenteredSlidesBreakpoint1', 'slider_legend', PaletteManipulator::POSITION_APPEND)
 
     ->addField('sliderBreakpoint2', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('sliderSlidesPerViewBreakpoint2', 'slider_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('sliderSlidesPerGroupBreakpoint2', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('sliderSpaceBetweenBreakpoint2', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('sliderCenteredSlidesBreakpoint2', 'slider_legend', PaletteManipulator::POSITION_APPEND)
 
     ->addField('sliderBreakpoint3', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('sliderSlidesPerViewBreakpoint3', 'slider_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('sliderSlidesPerGroupBreakpoint3', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('sliderSpaceBetweenBreakpoint3', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('sliderCenteredSlidesBreakpoint3', 'slider_legend', PaletteManipulator::POSITION_APPEND)
 
     ->addField('sliderBreakpoint4', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('sliderSlidesPerViewBreakpoint4', 'slider_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('sliderSlidesPerGroupBreakpoint4', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('sliderSpaceBetweenBreakpoint4', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('sliderCenteredSlidesBreakpoint4', 'slider_legend', PaletteManipulator::POSITION_APPEND)
     
@@ -37,7 +41,7 @@ PaletteManipulator::create()
 ;
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['sliderDelay']['eval']['rgxp'] = 'natural';
-$GLOBALS['TL_DCA']['tl_content']['fields']['sliderDelay']['default'] = 4000;
+$GLOBALS['TL_DCA']['tl_content']['fields']['sliderDelay']['default'] = 5000;
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['sliderSpeed']['eval']['rgxp'] = 'natural';
 $GLOBALS['TL_DCA']['tl_content']['fields']['sliderSpeed']['default'] = 1500;
@@ -54,6 +58,15 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['sliderBreakpoint1'] = [
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['sliderSlidesPerViewBreakpoint1'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint1'],
+    'exclude' => true,
+    'default' => 1,
+    'inputType' => 'text',
+    'eval' => ['maxlength' => 4, 'tl_class' => 'w25'],
+    'sql' => "varchar(4) NOT NULL default '1'",
+];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['sliderSlidesPerGroupBreakpoint1'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerGroupBreakpoint1'],
     'exclude' => true,
     'default' => 1,
     'inputType' => 'text',
@@ -83,12 +96,21 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['sliderBreakpoint2'] = [
     'exclude' => true,
     'default' => 768,
     'inputType' => 'text',
-    'eval' => ['maxlength' => 4, 'tl_class' => 'w25'],
+    'eval' => ['maxlength' => 4, 'tl_class' => 'w25 clr'],
     'sql' => "varchar(4) NOT NULL default '768'",
 ];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['sliderSlidesPerViewBreakpoint2'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint2'],
+    'exclude' => true,
+    'default' => 1,
+    'inputType' => 'text',
+    'eval' => ['maxlength' => 4, 'tl_class' => 'w25'],
+    'sql' => "varchar(4) NOT NULL default '1'",
+];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['sliderSlidesPerGroupBreakpoint2'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerGroupBreakpoint2'],
     'exclude' => true,
     'default' => 1,
     'inputType' => 'text',
@@ -118,12 +140,21 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['sliderBreakpoint3'] = [
     'exclude' => true,
     'default' => 1024,
     'inputType' => 'text',
-    'eval' => ['maxlength' => 4, 'tl_class' => 'w25'],
+    'eval' => ['maxlength' => 4, 'tl_class' => 'w25 clr'],
     'sql' => "varchar(4) NOT NULL default '1024'",
 ];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['sliderSlidesPerViewBreakpoint3'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint3'],
+    'exclude' => true,
+    'default' => 1,
+    'inputType' => 'text',
+    'eval' => ['maxlength' => 4, 'tl_class' => 'w25'],
+    'sql' => "varchar(4) NOT NULL default '1'",
+];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['sliderSlidesPerGroupBreakpoint3'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerGroupBreakpoint3'],
     'exclude' => true,
     'default' => 1,
     'inputType' => 'text',
@@ -153,12 +184,21 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['sliderBreakpoint4'] = [
     'exclude' => true,
     'default' => 1441,
     'inputType' => 'text',
-    'eval' => ['maxlength' => 4, 'tl_class' => 'w25'],
+    'eval' => ['maxlength' => 4, 'tl_class' => 'w25 clr'],
     'sql' => "varchar(4) NOT NULL default '1441'",
 ];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['sliderSlidesPerViewBreakpoint4'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint4'],
+    'exclude' => true,
+    'default' => 1,
+    'inputType' => 'text',
+    'eval' => ['maxlength' => 4, 'tl_class' => 'w25'],
+    'sql' => "varchar(4) NOT NULL default '1'",
+];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['sliderSlidesPerGroupBreakpoint4'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerGroupBreakpoint4'],
     'exclude' => true,
     'default' => 1,
     'inputType' => 'text',
@@ -224,6 +264,30 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['sliderPagination'] = [
     'sql' => "char(1) NOT NULL default ''",
 ];
 
+$GLOBALS['TL_DCA']['tl_content']['fields']['sliderBreakpoints'] = [
+    'exclude' => true,
+    'inputType' => 'group',
+    'palette' => ['breakpoint', 'slidesPerView', 'spaceBetween'],
+    'eval' => ['tl_class' => 'clr'],
+    'fields' => [
+        'breakpoint' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['sliderBreakpointsSettings']['breakpoint'],
+            'inputType' => 'text',
+            'eval' => ['rgxp' => 'natural', 'tl_class' => 'w50'],
+        ],
+        'slidesPerView' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['sliderBreakpointsSettings']['slidesPerView'],
+            'inputType' => 'text',
+            'eval' => ['maxlength' => 4, 'tl_class' => 'clr w50'],
+        ],
+        'spaceBetween' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['sliderBreakpointsSettings']['spaceBetween'],
+            'inputType' => 'text',
+            'eval' => ['rgxp' => 'digit', 'tl_class' => 'w50'],
+        ],
+    ],
+    'sql' => 'BLOB null',
+];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['sliderAutoheight'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_content']['sliderAutoheight'],
