844385857c33cc67d34708105361875b128daa43
Niels <info@heimseiten.de>
2024-06-02 11:03:15 +0200

    Add files via upload

---
+++ contao/languages/de/tl_content.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['tl_content']['sliderEffect'] = ['Effekt', 'Animation beim Wechsel der Slides.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderButtons'] = ['Steuerschaltflächen', 'Zeigt Vor- und Zurück-Schaltflächen an.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderScrollbar'] = ['Scroll-Balken', 'Zeigt einen Scroll-Balken an.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderPagination'] = ['Navigationspunkte', 'Zeigt Navigationspunkte für die einzelnen Slides an.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderAutoheight'] = ['Automatische Höhe', 'Wenn aktiv, passt sich die Höhe des Sliders an die Höhe des aktuell aktiven Elements an.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderKeyboardControl'] = ['Tastatursteuerung', 'Ermöglicht die Navigation der Slides mit den Pfeiltasten.'];
+
+$GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint1'] = ['Breite des Fensters >=', "In Pixel."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint1'] = ['Anzahl der sichtbaren Slides', "Die Anzahl an einzelnen Slides die pro Sektion sichtbar sein soll (Zahl oder 'auto')."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint1'] = ['Abstand zwischen den Slides', 'Abstand zwischen den Slides in Pixel.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint1'] = ['Zentrierte Slides', 'Zentriert den gerade aktiven Slide, anstatt ihn links auszurichten.'];
+
+$GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint2'] = ['Breite des Fensters >=', "In Pixel."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint2'] = ['Anzahl der sichtbaren Slides', "Die Anzahl an einzelnen Slides die pro Sektion sichtbar sein soll (Zahl oder 'auto')."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint2'] = ['Abstand zwischen den Slides', 'Abstand zwischen den Slides in Pixel.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint2'] = ['Zentrierte Slides', 'Zentriert den gerade aktiven Slide, anstatt ihn links auszurichten.'];
+
+$GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint3'] = ['Breite des Fensters >=', "In Pixel."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint3'] = ['Anzahl der sichtbaren Slides', "Die Anzahl an einzelnen Slides die pro Sektion sichtbar sein soll (Zahl oder 'auto')."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint3'] = ['Abstand zwischen den Slides', 'Abstand zwischen den Slides in Pixel.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint3'] = ['Zentrierte Slides', 'Zentriert den gerade aktiven Slide, anstatt ihn links auszurichten.'];
+
+$GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint4'] = ['Breite des Fensters >=', "In Pixel."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint4'] = ['Anzahl der sichtbaren Slides', "Die Anzahl an einzelnen Slides die pro Sektion sichtbar sein soll (Zahl oder 'auto')."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint4'] = ['Abstand zwischen den Slides', 'Abstand zwischen den Slides in Pixel.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint4'] = ['Zentrierte Slides', 'Zentriert den gerade aktiven Slide, anstatt ihn links auszurichten.'];
---
+++ contao/languages/en/tl_content.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['tl_content']['sliderEffect'] = ['Effect', 'The slide animation.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderButtons'] = ['Prev/next buttons', 'Shows the prev/next buttons.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderScrollbar'] = ['Scrollbar', 'Shows a dragabble scrollbar.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderPagination'] = ['Pagination', 'Shows a clickable pagination to navigate the slides.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderAutoheight'] = ['Auto-height', 'When active the height of the slider adapts to the height of the currently active element.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderKeyboardControl'] = ['Keyboard control', 'Enables the arrow keys to navigate the slides.'];
+
+$GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint1'] = ['Width of the window >=', "In pixels."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint1'] = ['Slides per view', "Number of visible slides (number or 'auto')."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint1'] = ['Space between', 'Space between slides in pixels.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint1'] = ['Centered slides', 'When active the current slide will be in the center of the slider rathern than aligned to the left.'];
+
+$GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint2'] = ['Width of the window >=', "In pixels."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint2'] = ['Slides per view', "Number of visible slides (number or 'auto')."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint2'] = ['Space between', 'Space between slides in pixels.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint2'] = ['Centered slides', 'When active the current slide will be in the center of the slider rathern than aligned to the left.'];
+
+$GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint3'] = ['Width of the window >=', "In pixels."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint3'] = ['Slides per view', "Number of visible slides (number or 'auto')."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint3'] = ['Space between', 'Space between slides in pixels.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint3'] = ['Centered slides', 'When active the current slide will be in the center of the slider rathern than aligned to the left.'];
+
+$GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint4'] = ['Width of the window >=', "In pixels."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint4'] = ['Slides per view', "Number of visible slides (number or 'auto')."];
+$GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint4'] = ['Space between', 'Space between slides in pixels.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint4'] = ['Centered slides', 'When active the current slide will be in the center of the slider rathern than aligned to the left.'];
