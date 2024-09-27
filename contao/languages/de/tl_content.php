b2e913212e1599db633a470f4109912f867a0cd7
Niels <info@heimseiten.de>
2024-09-27 10:31:55 +0200

    Update tl_content.php

--- contao/languages/de/tl_content.php
+++ contao/languages/de/tl_content.php
@@ -14,18 +14,22 @@ $GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint1'] = ['Breite des Fensters >
 $GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint1'] = ['Anzahl der sichtbaren Slides', "Die Anzahl an einzelnen Slides die pro Sektion sichtbar sein soll (Zahl oder 'auto')."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint1'] = ['Abstand zwischen den Slides', 'Abstand zwischen den Slides in Pixel.'];
 $GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerGroupBreakpoint1'] = ['Anzahl gruppierter Slides', 'Bestimmt, wie viele Einträge gleichzeitig verschoben werden.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint1'] = ['zentierte Slides', ''];
 
 $GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint2'] = ['Breite des Fensters >=', "In Pixel."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint2'] = ['Anzahl der sichtbaren Slides', "Die Anzahl an einzelnen Slides die pro Sektion sichtbar sein soll (Zahl oder 'auto')."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint2'] = ['Abstand zwischen den Slides', 'Abstand zwischen den Slides in Pixel.'];
-$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint2'] = ['Anzahl gruppierter Slides', 'Bestimmt, wie viele Einträge gleichzeitig verschoben werden.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerGroupBreakpoint2'] = ['Anzahl gruppierter Slides', 'Bestimmt, wie viele Einträge gleichzeitig verschoben werden.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint2'] = ['zentierte Slides', ''];
 
 $GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint3'] = ['Breite des Fensters >=', "In Pixel."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint3'] = ['Anzahl der sichtbaren Slides', "Die Anzahl an einzelnen Slides die pro Sektion sichtbar sein soll (Zahl oder 'auto')."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint3'] = ['Abstand zwischen den Slides', 'Abstand zwischen den Slides in Pixel.'];
-$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint3'] = ['Anzahl gruppierter Slides', 'Bestimmt, wie viele Einträge gleichzeitig verschoben werden.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerGroupBreakpoint3'] = ['Anzahl gruppierter Slides', 'Bestimmt, wie viele Einträge gleichzeitig verschoben werden.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint3'] = ['zentierte Slides', ''];
 
 $GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint4'] = ['Breite des Fensters >=', "In Pixel."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint4'] = ['Anzahl der sichtbaren Slides', "Die Anzahl an einzelnen Slides die pro Sektion sichtbar sein soll (Zahl oder 'auto')."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint4'] = ['Abstand zwischen den Slides', 'Abstand zwischen den Slides in Pixel.'];
-$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint4'] = ['Anzahl gruppierter Slides', 'Bestimmt, wie viele Einträge gleichzeitig verschoben werden.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerGroupBreakpoint4'] = ['Anzahl gruppierter Slides', 'Bestimmt, wie viele Einträge gleichzeitig verschoben werden.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint4'] = ['zentierte Slides', ''];
