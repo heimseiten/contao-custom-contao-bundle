dcbd8ca2b29600f490f21e070023b28e832d9650
Niels <info@heimseiten.de>
2024-09-27 10:32:10 +0200

    Update tl_content.php

--- contao/languages/en/tl_content.php
+++ contao/languages/en/tl_content.php
@@ -13,19 +13,23 @@ $GLOBALS['TL_LANG']['tl_content']['sliderVertical'] = ['swipe vertical', '!!! Fo
 $GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint1'] = ['Width of the window >=', "In pixels."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint1'] = ['Slides per view', "Number of visible slides (number or 'auto')."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint1'] = ['Space between', 'Space between slides in pixels.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerGroupBreakpoint1'] = ['Number of grouped slides', 'Determines how many entries are moved at the same time.'];
 $GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint1'] = ['Centered slides', 'When active the current slide will be in the center of the slider rathern than aligned to the left.'];
 
 $GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint2'] = ['Width of the window >=', "In pixels."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint2'] = ['Slides per view', "Number of visible slides (number or 'auto')."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint2'] = ['Space between', 'Space between slides in pixels.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerGroupBreakpoint2'] = ['Number of grouped slides', 'Determines how many entries are moved at the same time.'];
 $GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint2'] = ['Centered slides', 'When active the current slide will be in the center of the slider rathern than aligned to the left.'];
 
 $GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint3'] = ['Width of the window >=', "In pixels."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint3'] = ['Slides per view', "Number of visible slides (number or 'auto')."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint3'] = ['Space between', 'Space between slides in pixels.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerGroupBreakpoint3'] = ['Number of grouped slides', 'Determines how many entries are moved at the same time.'];
 $GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint3'] = ['Centered slides', 'When active the current slide will be in the center of the slider rathern than aligned to the left.'];
 
 $GLOBALS['TL_LANG']['tl_content']['sliderBreakpoint4'] = ['Width of the window >=', "In pixels."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerViewBreakpoint4'] = ['Slides per view', "Number of visible slides (number or 'auto')."];
 $GLOBALS['TL_LANG']['tl_content']['sliderSpaceBetweenBreakpoint4'] = ['Space between', 'Space between slides in pixels.'];
+$GLOBALS['TL_LANG']['tl_content']['sliderSlidesPerGroupBreakpoint4'] = ['Number of grouped slides', 'Determines how many entries are moved at the same time.'];
 $GLOBALS['TL_LANG']['tl_content']['sliderCenteredSlidesBreakpoint4'] = ['Centered slides', 'When active the current slide will be in the center of the slider rathern than aligned to the left.'];
