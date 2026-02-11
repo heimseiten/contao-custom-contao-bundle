cda067473f5be43cc3974540c517bcb03ca8e4cf
Niels <info@heimseiten.de>
2026-02-11 08:30:12 +0100

    Remove conditional JavaScript loading for backend
    Remove backend request check for loading JavaScript.

--- contao/config/config.php
+++ contao/config/config.php
@@ -1,7 +1,1 @@
 <?php
-use Contao\System;
-use Symfony\Component\HttpFoundation\Request;
-
-if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
-    $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/heimseitencontaocustomcontao/focus_the_field_name_on_load.js|static';
-}
