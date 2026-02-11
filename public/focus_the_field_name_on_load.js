d5c270122370da00f7f1a6358302015abb5ba7b5
Niels <info@heimseiten.de>
2026-02-11 08:29:53 +0100

    Delete public/focus_the_field_name_on_load.js

--- public/focus_the_field_name_on_load.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('DOM fully loaded and parsed');
-    if (document.querySelector("#ctrl_title")) {
-        document.querySelector("#ctrl_title").focus()
-    }
-})
