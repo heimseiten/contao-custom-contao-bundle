3c1cfbc0497210ee39800d0c1cbb446013873522
Niels <info@heimseiten.de>
2026-06-18 12:02:25 +0200

    Galerie-Checkbox: benötigte CSS-Klasse + Modifier in der Hilfe ergänzt

--- contao/languages/de/tl_layout.php
+++ contao/languages/de/tl_layout.php
@@ -6,4 +6,4 @@ $GLOBALS['TL_LANG']['tl_layout']['cc_scroll_direction']            = ['Scroll-Ri
 $GLOBALS['TL_LANG']['tl_layout']['cc_add_header_height']           = ['Header-Höhe als CSS-Variable', 'Setzt --header_height und --header_top_height am Body (z. B. für Sticky-Layouts).'];
 $GLOBALS['TL_LANG']['tl_layout']['cc_scroll_to_error']             = ['Zu Formularfehlern scrollen', 'Scrollt nach dem Absenden automatisch zum ersten Formularfehler.'];
 $GLOBALS['TL_LANG']['tl_layout']['cc_remove_body_class_preload']   = ['"preload"-Klasse entfernen', 'Entfernt die Body-Klasse "preload", sobald die Seite geladen ist.'];
-$GLOBALS['TL_LANG']['tl_layout']['cc_horizontale_slide_animation'] = ['Galerie: horizontale Slide-Animation', 'Lädt JS + CSS für die horizontale Slide-Animation der Galerie.'];
+$GLOBALS['TL_LANG']['tl_layout']['cc_horizontale_slide_animation'] = ['Galerie: horizontale Slide-Animation', 'Lädt JS + CSS für die horizontale Slide-Animation. Das Galerie-Element braucht die CSS-Klasse "horizontaleSlideAnimation" (optional zusätzlich "GeschwindigkeitLangsam"/"GeschwindigkeitSchnell" und "RichtungLinks"/"RichtungRechts").'];
